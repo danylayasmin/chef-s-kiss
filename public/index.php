4d9d7f73c8ec6373c7e6d0ae638ba0b4b4311786
Nyl <166322@student.horizoncollege.nl>
2023-03-23 09:54:05 +0100

    twig install

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo("Hello Framework");
+require_once '../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../views');
+$twig = new \Twig\Environment($loader);
+echo $twig->render('welcome.twig');
